02d68deac75ae34b40748a98c0369fe210742fc4
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-11-11 20:12:35 -0300

    feat: create data-source

---
+++ src/data-source.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+// Definir estas variáveis de ambiente e seus valores em um arquivo .env na raíz do projeto
+const host = process.env.DB_HOST 
+const port = parseInt(process.env.DB_PORT);
+const username = process.env.DB_USERNAME
+const password = process.env.DB_PASSWORD
+const database = process.env.DB_DATABASE
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host,
+    port,
+    username,
+    password,
+    database,
+    synchronize: true,
+    logging: true,
+    entities: [/* Feira, Departamento, .... demais classes mapeadas para o banco ficam aqui*/],
+    migrations: [],
+    subscribers: [],
+})
