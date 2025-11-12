91bd92fe812ed771954afd24b0c06b4c45c3fbc7
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-11-12 16:12:47 -0300

    chore: rename DB_DATABASE to DB_NAME

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,7 @@ const host = process.env.DB_HOST
 const port = parseInt(process.env.DB_PORT);
 const username = process.env.DB_USERNAME
 const password = process.env.DB_PASSWORD
-const database = process.env.DB_DATABASE
+const database = process.env.DB_NAME
 
 export const AppDataSource = new DataSource({
     type: "postgres",
