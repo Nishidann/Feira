756e6d87321a3b98daae15d67526f49b8492f212
Rafael Roseira Machado <rafaroseira27@outlook.com>
2026-01-14 12:09:50 -0300

    feat: create auth router

---
+++ src/routes/AuthRouter.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response } from "express";
+import { AuthController } from "../controllers/AuthController";
+
+const authRouter = Router();
+const authController = new AuthController();
+
+authRouter.post("/login", (req: Request, res: Response) => {
+    authController.login(req, res);
+});
+
+export default authRouter;
